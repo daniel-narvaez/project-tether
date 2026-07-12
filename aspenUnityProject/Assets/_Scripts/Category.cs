ee4543c13b9c2cf8da9100d45824aa64c5f916bb
Daniel Narvaez <github.uabx3@passfwd.com>
2026-07-12 12:26:23 -0400

    duplicates removed

--- aspenUnityProject/Assets/_Scripts/Category.cs
+++
@@ -1,8 +0,0 @@
-/// <summary>
-/// The different kinds of categories for items. Used for filtering. Add/Remove categories as needed.
-/// </summary>
-public enum Category
-{
-    Miscellaneous = 0,
-    Consumables = 1
-}
