0b5164ea82cf1246f03d80e6ba87b848abca2315
Jason Padilla <Jasonpadilla104@gmail.com>
2026-07-24 23:22:46 -0400

    modified: Tile System V0.5

--- aspenUnityProject/Assets/_Scripts/Runtime/Hex Grid System/Scripts/TilePlacementManager.cs
+++ aspenUnityProject/Assets/_Scripts/Runtime/Hex Grid System/Scripts/TilePlacementManager.cs
@@ -1,11 +1,52 @@
 using AYellowpaper.SerializedCollections;
-using UnityEngine;
+using System;
 using TileSystem;
+using UnityEditor;
+using UnityEngine;
+
 
 /* IF we need to optimize, create a tile object pool that holds tile creation data,
  * stores them in a dict and looksup tile data whenever its required
  * 
  */
+
+[CustomEditor(typeof(TilePlacementManager))]
+public class CustomTilePlacementManagerEditor : Editor
+{
+    SerializedProperty _allTilesTypes;
+
+    private void OnEnable()
+    {
+        _allTilesTypes = serializedObject.FindProperty("_allTilesTypes");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        // Draw EVERYTHING except "_allTilesTypes"
+        DrawPropertiesExcluding(serializedObject, "_allTilesTypes");
+
+        EditorGUILayout.Space();
+
+        DrawTiles();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    private void DrawTiles()
+    {
+        EditorGUILayout.LabelField("Tiles", EditorStyles.boldLabel);
+
+        for (int i = 0; i < _allTilesTypes.arraySize; i++)
+        {
+            EditorGUILayout.PropertyField(
+                _allTilesTypes.GetArrayElementAtIndex(i),
+                new GUIContent($"Tile {i + 1}")
+            );
+        }
+    }
+}
 
 
 public class TilePlacementManager : MonoBehaviour
@@ -17,7 +58,28 @@ public class TilePlacementManager : MonoBehaviour
     [SerializeField] Material _lavaMAT;
     #endregion
 
-    public SerializedDictionary<GameObject, TileType> _tileDict = new();
+    SerializedDictionary<Transform, TileType> _tileDict = new();
+
+    const int requiredSize = 19;
+    [SerializeField] 
+    TileType[] _allTilesTypes = new TileType[requiredSize];
+
+    [SerializeField] GameObject _allTiles;
+
+
+    private void OnValidate()
+    {
+        if (_allTilesTypes.Length != requiredSize)
+        {
+            Debug.LogWarning($"Array size is locked to {requiredSize}!");
+            Array.Resize(ref _allTilesTypes, requiredSize);
+        }
+
+        if (_allTiles == null)
+        {
+            Debug.LogWarning("_allTiles field is empty");
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,6 +89,11 @@ public class TilePlacementManager : MonoBehaviour
 
     void GenerateAllTiles()
     {
+        for (int i = 0, j = 0; i < _allTilesTypes.Length && j < _allTiles.transform.childCount; i++, j++)
+        {
+            _tileDict.Add(_allTiles.transform.GetChild(j), _allTilesTypes[i]);
+        }
+
         foreach (var (tile, tileType) in _tileDict)
         {
             switch (tileType)
@@ -41,7 +108,7 @@ public class TilePlacementManager : MonoBehaviour
         }
     }
 
-    void SetTileInfo(GameObject tile, Tile tileInfo)
+    void SetTileInfo(Transform tile, Tile tileInfo)
     {
         tile.GetComponent<TileContainer>().SetTile(tileInfo);
         tile.GetComponent<MeshFilter>().mesh = tileInfo.Mesh;
@@ -52,7 +119,14 @@ public class TilePlacementManager : MonoBehaviour
     {
         if(!Application.isPlaying)
         {
-            foreach (var (tile, tileType) in _tileDict)
+            SerializedDictionary<Transform, TileType> dict = new();
+
+            for (int i = 0, j = 0; i < _allTilesTypes.Length && j < _allTiles.transform.childCount; i++, j++)
+            {
+                dict.Add(_allTiles.transform.GetChild(j), _allTilesTypes[i]);
+            }
+
+            foreach (var (tile, tileType) in dict)
             {
                 switch (tileType)
                 {
