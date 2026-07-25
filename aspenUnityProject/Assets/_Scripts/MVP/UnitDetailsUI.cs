4f20d87fcfb9e085680b5cef229de8f7ada5cada
Daniel Narvaez <github.uabx3@passfwd.com>
2026-07-25 18:17:45 -0400

    unit details update

--- aspenUnityProject/Assets/_Scripts/MVP/UnitDetailsUI.cs
+++ aspenUnityProject/Assets/_Scripts/MVP/UnitDetailsUI.cs
@@ -1,28 +1,33 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitDetailsUI : MonoBehaviour
 {
-  private UnitDataSO _unitData;
   [SerializeField] private TextMeshProUGUI _nameText;
+  [Space(10)]
   [SerializeField] private TextMeshProUGUI _levelText;
+  [SerializeField] private Slider _levelSlider;
+  [Space(10)]
   [SerializeField] private TextMeshProUGUI _aptitudesText;
   [Space(10)]
   [SerializeField] private List<TextMeshProUGUI> _statTexts;
   [Space (10)]
   [SerializeField] private List<TextMeshProUGUI> _equipmentTexts;
 
+  private UnitDataSO _unitData;
   private bool _showingAptitudes = false;
+  private Dictionary<Stat, int> statData = new Dictionary<Stat, int>();
 
   public void DisplayUnitDetails(Entity entity)
   {
     _unitData = entity.UnitData ? entity.UnitData : null;
-
     if(_unitData)
     {
       _nameText.text = _unitData.Name;
       _levelText.text = $"Lv.{_unitData.Level}";
+      _levelSlider.value = _unitData.Level; 
 
       // The TMP's MUST be in the same order that the stats are ordered!
       for (int i = 0; i < _statTexts.Count; i++)
@@ -30,7 +35,21 @@ public class UnitDetailsUI : MonoBehaviour
         Stat stat = (Stat)i;
         int val = Formulae.CalculateStat(stat, _unitData.AllStats[stat], _unitData.Level);
         _statTexts[i].text = val.ToString();
+        statData.Add(stat, val);
       }
+    }
+  }
+
+  public void UpdateLevel()
+  {
+    _unitData.Level = (int)_levelSlider.value;
+    _levelText.text = $"Lv.{_unitData.Level}";
+    for (int i = 0; i < _statTexts.Count; i++)
+    {
+      Stat stat = (Stat)i;
+      int val = Formulae.CalculateStat(stat, _unitData.AllStats[stat], _unitData.Level);
+      _statTexts[i].text = val.ToString();
+      statData[stat] = val;
     }
   }
 
@@ -38,6 +57,9 @@ public class UnitDetailsUI : MonoBehaviour
   {
     _nameText.text = "Name";
     _levelText.text = "Lv.1";
+    _levelSlider.value = 1;
+
+    statData.Clear();
 
     foreach( TextMeshProUGUI tmp in _statTexts)
       tmp.text = 0.ToString();
@@ -67,8 +89,7 @@ public class UnitDetailsUI : MonoBehaviour
         for (int i = 0; i < _statTexts.Count; i++)
         {
           Stat stat = (Stat)i;
-          int val = Formulae.CalculateStat(stat, _unitData.AllStats[stat], _unitData.Level);
-          _statTexts[i].text = val.ToString();
+          _statTexts[i].text = statData[stat].ToString();
         }
         _aptitudesText.text = "Show Aptitudes";
         _showingAptitudes = false;
