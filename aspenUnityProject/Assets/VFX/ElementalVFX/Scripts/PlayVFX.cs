ea7b2dc504735c9eba23a56d04a8b60925bbfbeb
earthbound0 <emmanuelbusumuru5742@gmail.com>
2026-08-12 11:35:08 -0400

    Some VFX added

--- aspenUnityProject/Assets/VFX/ElementalVFX/Scripts/PlayVFX.cs
+++ aspenUnityProject/Assets/VFX/ElementalVFX/Scripts/PlayVFX.cs
@@ -1,17 +1,18 @@
-using Mono.Cecil;
 using UnityEngine;
+using UnityEngine.VFX; // <---- Use this library.
 using UnityEngine.InputSystem;
 
-public class Burst : MonoBehaviour
+
+public class PlayVFX : MonoBehaviour
 {
-    // public ParticleSystem.Particle ps;
-    public Object burstPrefab;
+    public VisualEffect vfx; // <---- Create a VisualEffect object. 
     public InputActionReference trigger;
+    
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // ps = GetComponent<ParticleSystem.Particle>();
+        
     }
 
     // Update is called once per frame
@@ -19,7 +20,7 @@ public class Burst : MonoBehaviour
     {
         
     }
-
+    
     void OnEnable()
     {
         trigger.action.started += Space;
@@ -30,9 +31,11 @@ public class Burst : MonoBehaviour
         trigger.action.started -= Space; // To avoid and action like 'Space' being squiggled, create a method of the same name down below.
     }
 
+    
     private void Space(InputAction.CallbackContext context) // A method of the Action you created is needed down here. 
     {
         Debug.Log("Space");
-        Instantiate(burstPrefab);
+        vfx.Play(); // <---- Call the .Play function from the VisualEffect class. 
+        
     }
 }
