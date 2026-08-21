ed16037f56d35e1d0ebb0bf3685a53d86ad9b625
Jason Padilla <Jasonpadilla104@gmail.com>
2026-08-21 15:08:19 -0400

    modified: Tile System cleanup + some documentation

--- aspenUnityProject/Assets/_Scripts/Runtime/Tile System/Scripts/TileContainer.cs
+++ aspenUnityProject/Assets/_Scripts/Runtime/Tile System/Scripts/TileContainer.cs
@@ -9,13 +9,13 @@ public class TileContainer : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SetTile(Tile tile) => _tileProp = tile;
